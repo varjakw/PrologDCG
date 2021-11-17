45a6fe73e77e16939b10091e9293ef758648a982
Varjak Wolfe <78870995+varjakw@users.noreply.github.com>
2021-11-17 14:46:38 +0000

    Update basics.pl

--- basics.pl
+++ basics.pl
@@ -11,3 +11,19 @@ det([a]).
 n([man]).
 n([woman]).
 v([shoots]).
+
+%example query:
+
+%?- s([the, woman, shoots, a , man)]
+%yes
+
+%This means the string "the woman shoots a man" is a grammatically correct sentence for the given CFG
+
+%We can also ask prolog to list all the possible valid sentences:
+
+%?- s(S)
+
+%S = [the, woman, shoots, a , man]
+%S = [the, woman, shoots, the , man]
+%S = [the, man, shoots, a , woman]
+%....and so on
