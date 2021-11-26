29a47412e379247be7e7537e1796f9922ae1de7d
Varjak Wolfe <78870995+varjakw@users.noreply.github.com>
2021-11-26 13:32:46 +0000

    Update DCG-FormLang.pl

--- DCG-FormLang.pl
+++ DCG-FormLang.pl
@@ -1,4 +1,4 @@
-%We will define the language a^n b^n
+% We will define the language a^n b^n
 
 s --> [].#
 s--> l,s,r.
@@ -10,3 +10,9 @@ r --> [b].
 
 %?- s([a,a,a,a,b,b,b],[]).
 % no
+
+%? - s(X,[])
+% X = [];
+% X = [a,b];
+% X = [a,a,b,b];
+%....so on
