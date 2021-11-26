63fd8c7b5fec3a0b0e5c010cde3cb4696d4ff5ce
Varjak Wolfe <78870995+varjakw@users.noreply.github.com>
2021-11-26 16:01:20 +0000

    Update DCG-FormLang.pl

--- DCG-FormLang.pl
+++ DCG-FormLang.pl
@@ -1,3 +1,12 @@
+% The formal language "Even" is very sim[;e, consisting of all strings containing
+% an even number of as and nothing else. Note that the empty string belongs
+% to "Even".
+
+s --> [].
+s --> [a,a],s.
+
+% We can see that s in line 6 will be replaced by another two as and so on.
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % We will define the language a^n b^n
 
 s --> [].
@@ -16,3 +25,18 @@ r --> [b].
 % X = [a,b];
 % X = [a,a,b,b];
 %....so on
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%The formal language a^n b^2m, c^2m, d^n consits of all strings of form 
+% where the number of a's and d's is equal and the number of b's and c's
+%is even and equal. e.g. abbccd, aabbbbccccdd.
+
+%A DCG to generate this language:
+
+s --> m.
+s --> [a], s, [d].
+m --> [].
+m --> [b,b], m, [c,c].
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%
