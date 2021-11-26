ae25e53ef5b108f90c150c8a50d0568dfc338e22
Varjak Wolfe <78870995+varjakw@users.noreply.github.com>
2021-11-26 15:01:17 +0000

    Update DCG-ExtraArgs.pl

--- DCG-ExtraArgs.pl
+++ DCG-ExtraArgs.pl
@@ -3,20 +3,20 @@
 % she and he are subject pronouns while her and him are obejct pronouns.
 % We can do this using extra arguments.
 
-s $-->$ np(subject), vp. 
-np(_) $-->$ det, n. 
-np(X) $-->$ pro(X).
-vp $-->$ v, np(object). 
-vp $-->$ v. 
-det $-->$ [the].     
-det $-->$ [a]. 
-n $-->$ [man].    
-n $-->$ [woman].   
-v $-->$ [shoots].
-pro(subject) $-->$ [he].
-pro(subject) $-->$  [she].
-pro(object) $-->$ [him].
-pro(object) $-->$ [her].
+s --> np(subject), vp. 
+np(_) --> det, n. 
+np(X) --> pro(X).
+vp --> v, np(object). 
+vp --> v. 
+det --> [the].     
+det --> [a]. 
+n --> [man].    
+n --> [woman].   
+v --> [shoots].
+pro(subject) --> [he].
+pro(subject) -->  [she].
+pro(object) --> [him].
+pro(object) --> [her].
 
 %?- s([she, shoots, he],[])
 %no
